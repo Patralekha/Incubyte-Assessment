891c1e00f997e8befcfc2ae67c2bea1912ae3d6b
Patralekha Bhattacharjee <46372817+Patralekha@users.noreply.github.com>
2021-02-17 11:50:38 +0530

    Update run_on_remote.sh

--- run_on_remote.sh
+++ run_on_remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-git clone https://root:crio-test@gitlab.crio.do/COHORT_ME_BUILDOUT_XMEME_ENROLL_1612436694845/${usr}-me_buildout_xmeme
+git clone https://gitlab.crio.do/COHORT_ME_BUILDOUT_XMEME_ENROLL_1612436694845/${usr}-me_buildout_xmeme
 
 # Install python.
 sudo apt-get -y update; sudo apt install -y python3
